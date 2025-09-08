54ce390802fac8b78a502d9dcc66454896576e38
Sauher <schaueroliver@turr.hu>
2025-09-08 10:59:03 +0200

    Elso

---
+++ assets/js/stepcounter.js
@@ -0,0 +1,64 @@
+let theme = 'light';
+let main = document.querySelector('main');
+
+const AppTitle = "Lépésszámláló App"
+const Author = "13.A Szoftverfejlesztő"
+const Company = "Bajai SZC Türr István Technikum"
+
+let Apptitle = document.getElementById("Title")
+let author = document.getElementById("Author")
+let company = document.getElementById("Company")
+
+Apptitle.innerHTML = AppTitle
+author.innerHTML = Author
+company.innerHTML = Company
+
+let lightmodeBtn = document.getElementById("lightmodeBtn")
+let darkmodeBtn = document.getElementById("darkmodeBtn")
+
+lightmodeBtn.addEventListener('click', ()=>{
+    setTheme('light')
+    saveTheme('light')
+})
+
+darkmodeBtn.addEventListener('click', ()=>{
+    setTheme('dark')
+    saveTheme('dark')
+})
+
+function loadTheme(){
+    theme = 'light'
+    if(localStorage.getItem('SCTheme')){
+        theme = localStorage.getItem('SCTheme');
+        
+    }
+    setTheme(theme)
+}
+
+function saveTheme(theme){
+    localStorage.setItem('SCTheme', theme)
+}
+
+function setTheme(theme){
+    document.documentElement.setAttribute('data-bs-theme', theme)
+    setThemeBtn(theme)
+}
+
+function setThemeBtn(theme){
+    if(theme == 'light'){
+        lightmodeBtn.classList.add('hide')
+        darkmodeBtn.classList.remove('hide')
+    }
+    else{
+        darkmodeBtn.classList.add('hide')
+        lightmodeBtn.classList.remove('hide')
+        
+    }
+}
+
+async function Render(view){
+ main.innerHTML =await (await fetch(`views/${view}.html`)).text()
+}
+
+loadTheme()
+Render('login')
---
+++ controllers/userControl.js
@@ -0,0 +1,18 @@
+let passfield = document.getElementById('passField')
+let nameField = document.getElementById('nameField')
+let emailField = document.getElementById('emailField')
+let confirmpassField = document.getElementById('confirmpassField')
+
+function Registration(){
+    alert("AAAAAAAAAAAAAAAAAAAAAA")
+}
+
+function Login(){}
+
+function Logout(){}
+
+function getProfile(){}
+
+function UpdateProfile(){}
+
+function ChangePass(){}
